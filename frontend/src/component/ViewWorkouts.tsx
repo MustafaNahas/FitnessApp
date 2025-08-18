dbe0cf2846b86f2f0d50a266e866c106e4b445d0
yuliiabezkorovaina <yuliya.bezkorovayna@gmail.com>
2025-08-18 16:23:36 +0200

    add View GetWorkoutByID edit App.tsx edit ViewWorkouts.tsx

--- frontend/src/component/ViewWorkouts.tsx
+++ frontend/src/component/ViewWorkouts.tsx
@@ -3,10 +3,14 @@ import {useEffect, useState} from "react";
 import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {faDumbbell, faPersonBiking, faPersonRunning, faPersonWalking} from "@fortawesome/free-solid-svg-icons";
+import {useNavigate} from "react-router-dom";
 
 export default function ViewWorkouts(){
 
+    const nav = useNavigate();
     const [workouts, setWorkouts] = useState([{id: "", description: "", workoutName: ""}]);
+
+
     function loadTodos() {
         axios.get("/api/workouts")
             .then(res => {
@@ -18,16 +22,27 @@ export default function ViewWorkouts(){
             });
     }
 
+
+
     useEffect(() => {
         loadTodos();
     }, []);
+
+    function handleGetById(id: string) {
+        nav(`/workouts/${id}`);
+    }
+
 
     return(
         <div className={"ViewWorkouts"}>
             <h1>Workouts</h1>
             {workouts.map(workout => (
-                <div key={workout.id} className="workout-card">
+                <button key={workout.id} className="workout-card" onClick={() => handleGetById(workout.id)}
+                >
                     {workout.workoutName==="Running"?
+                        <h2><FontAwesomeIcon icon={faPersonRunning} /> {workout.workoutName}</h2>:null
+                    }
+                    {workout.workoutName==="Weight training"?
                         <h2><FontAwesomeIcon icon={faPersonRunning} /> {workout.workoutName}</h2>:null
                     }
                     {workout.workoutName==="Walking"?
@@ -36,12 +51,12 @@ export default function ViewWorkouts(){
                     {workout.workoutName==="Cycling"?
                         <h2><FontAwesomeIcon icon={faPersonBiking} /> {workout.workoutName}</h2>:null
                     }
-                    {workout.workoutName==="Weight training"?
+                    {workout.workoutName==="Lifting"?
                         <h2><FontAwesomeIcon icon={faDumbbell} /> {workout.workoutName}</h2>:null
                     }
 
                     <p><strong>Description:</strong> {workout.description}</p>
-                </div>
+                </button>
             ))}
         </div>
     )
