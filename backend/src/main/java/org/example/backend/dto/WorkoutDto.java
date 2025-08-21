aee29f9a08d47647de1843477084bacf3bf1ed72
Roottoot <syuleiman@gmail.com>
2025-08-21 18:01:37 +0200

    Update Workout

--- backend/src/main/java/org/example/backend/dto/WorkoutDto.java
+++ backend/src/main/java/org/example/backend/dto/WorkoutDto.java
@@ -1,4 +1,14 @@
 package org.example.backend.dto;
 import lombok.With;
+
+import java.sql.Date;
+import java.sql.Time;
+import java.util.Optional;
+
 @With
-public record WorkoutDto(String description, String workoutName) { }
+public record WorkoutDto(String workoutName,
+                         Optional<String> description,
+                         Optional<Date> date,
+                         Optional<Time> startTime,
+                         Optional<Boolean> favorite,
+                         Optional<Double> duration) { }
--- backend/src/main/java/org/example/backend/model/Workout.java
+++ backend/src/main/java/org/example/backend/model/Workout.java
@@ -2,8 +2,17 @@ package org.example.backend.model;
 
 import lombok.With;
 
+import java.sql.Date;
+import java.sql.Time;
+import java.util.Optional;
+
 @With
 public record Workout(String id,
-                      String description,
-                      String workoutName) {
+                      String workoutName,
+                      Optional<String> description,
+                      Optional<Date> date,
+                      Optional<Time> startTime,
+                      Optional<Boolean> favorite,
+                      Optional<Double> duration
+                      ) {
 }
