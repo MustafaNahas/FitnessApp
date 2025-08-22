4af936ec68acf004c135617d593f50fb2c220af2
MustafaNahas <99922508+MustafaNahas@users.noreply.github.com>
2025-08-22 12:10:19 +0200

    add Security part 1

--- frontend/src/component/Navbar.tsx
+++ frontend/src/component/Navbar.tsx
@@ -2,14 +2,29 @@ import '../App.css'
 import {useNavigate} from "react-router-dom";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faHouse, faPlus} from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
 
 
 export default function Navbar() {
     const nav = useNavigate();
+    const loadUser = () => {
+        axios.get('/api/auth/me')
+            .then(response => {
+                console.log(response.data)
+            })
+    }
+    function login() {
+        const host = window.location.host === 'localhost:5173' ? 'http://localhost:8080': window.location.origin
+
+        window.open(host + '/oauth2/authorization/github', '_self')
+        loadUser();
+    }
+
     return (
         <nav>
             <button onClick={()=>(nav("/workouts"))}>Home <FontAwesomeIcon icon={faHouse} /></button>
             <button onClick={()=>(nav("/workouts/add"))}>Add a workout <FontAwesomeIcon icon={faPlus} /></button>
+            <button onClick={login}>login</button>
         </nav>
     );
 }
