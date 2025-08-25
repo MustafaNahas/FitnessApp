0ac31bee16258c4186c5bbb725124cc6932f50fc
Roottoot <syuleiman@gmail.com>
2025-08-25 15:46:09 +0200

    Fixed conversion

--- backend/src/main/java/org/example/backend/dto/WorkoutDto.java
+++ backend/src/main/java/org/example/backend/dto/WorkoutDto.java
@@ -1,14 +1,11 @@
 package org.example.backend.dto;
 import lombok.With;
 
-import java.sql.Date;
-import java.sql.Time;
-
 //test
 @With
 public record WorkoutDto(String workoutName,
                          String description,
-                         Date date,
-                         Time startTime,
+                         String date,
+                         String startTime,
                          Boolean favorite,
                          Double duration) { }
