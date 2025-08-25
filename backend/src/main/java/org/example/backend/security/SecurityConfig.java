d45ca284ae9ac070e30612a5968147138c2b6e31
MustafaNahas <99922508+MustafaNahas@users.noreply.github.com>
2025-08-25 16:22:01 +0200

    Update SecurityConfig.java

--- backend/src/main/java/org/example/backend/security/SecurityConfig.java
+++ backend/src/main/java/org/example/backend/security/SecurityConfig.java
@@ -16,9 +16,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-               .csrf(csrf -> csrf
-        .ignoringRequestMatchers("/api/**") // API stateless
-    )
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(a -> a
                         .requestMatchers("/api/auth/me").authenticated()
                         .requestMatchers("/api/secured").authenticated()
