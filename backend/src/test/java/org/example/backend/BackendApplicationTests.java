a88f3bde8e6f3f184f33de76661e0e47fbc55bf4
MustafaNahas <99922508+MustafaNahas@users.noreply.github.com>
2025-08-25 14:25:47 +0200

    Update BackendApplicationTests.java

--- backend/src/test/java/org/example/backend/BackendApplicationTests.java
+++ backend/src/test/java/org/example/backend/BackendApplicationTests.java
@@ -3,7 +3,7 @@ package org.example.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "app.url=http://localhost")
 class BackendApplicationTests {
     @Test
     void contextLoads() {
